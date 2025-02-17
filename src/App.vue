5aa3546f7e301f9ef5f22e0a19b893c7fd6b6e7c
Landi <joaomarcospsnbr@gmail.com>
2025-02-17 05:09:59 -0300

    fix: nav bar margin too big

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@
     <img class="logo" src="/logo.png" alt="Backstage Produções">
     <div id="nav-icons">
       <a href="https://www.instagram.com/backstageproductionsbr/" target="_blank">
-        <v-icon name="bi-instagram" fill="000000" :scale="1.75" />
+        <v-icon name="bi-instagram" fill="000000" :scale="1.70" />
       </a>
       <a href="https://www.facebook.com/people/Backstage-Productions-Br/61558564295158/" target="_blank">
-        <v-icon name="bi-facebook" fill="000000" :scale="1.75" />
+        <v-icon name="bi-facebook" fill="000000" :scale="1.70" />
       </a>
       <a href="https://www.linkedin.com/company/backstage-productions-uk/" target="_blank">
-        <v-icon name="co-linkedin-in" fill="000000" :scale="2" />
+        <v-icon name="co-linkedin-in" fill="000000" :scale="1.70" />
       </a>
       <a href="https://open.spotify.com/user/manituszerman?si=167cc7befe4e4e3b&nd=1&dlsi=2508b25e78484d03" target="_blank">
-        <v-icon name="bi-spotify" fill="000000" :scale="2" />
+        <v-icon name="bi-spotify" fill="000000" :scale="1.70" />
       </a>
     </div>
   </nav>
@@ -48,7 +48,7 @@
     display: flex;
     justify-content: center;
     align-items: end;
-    gap: 15px;
+    gap: 10px;
   }
 
   .logo {
